c14c05b845fc9e1bd5d2202bda710ee6cb4c32ce
Michael Holthouser <mholthouser.co@gmail.com>
2017-09-18 17:17:26 -0600

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,27 @@
+public class GasolineReceipt {
+    public static void main( String[] args ) {
+        System.out.println( "+-------------------------+");
+
+        System.out.println( "|                         |");
+
+        System.out.println( "|      CORNER STORE       |");
+
+        System.out.println( "|                         |");
+
+        System.out.println( "|  2017-09-16  04:38PM    |");
+
+        System.out.println( "|                         |");
+
+        System.out.println( "|   Gallons:     10.870   |");
+
+        System.out.println( "| Price/gallon: $ 2.49    |");
+
+        System.out.println( "|                         |");
+
+        System.out.println( "|  Fuel total:  $ 27.07   |");
+
+        System.out.println( "|                         |");
+
+        System.out.println( "+-------------------------+");
+    }
+}

9a5e314c69cd75ddb5f361bb438a6791a7c422c9
Michael Holthouser <mholthouser.co@gmail.com>
2017-09-18 17:13:02 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+    public static void main( String[] args ) {
+        System.out.println( "I, Michael Holthouser, am determined to learn how to code." );
+
+        System.out.println( "Today's date is Saturday, September, 16, 2017." );
+    }
+}

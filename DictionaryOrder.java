4baee53742ee0775ee059d27dab85eaea6a5ba67
Michael Holthouser <mholthouser.co@gmail.com>
2017-09-20 09:30:26 -0600

    Update DictionaryOrder.java

--- DictionaryOrder.java
+++ DictionaryOrder.java
@@ -8,9 +8,70 @@ public class DictionaryOrder {
     System.out.print( "Make up the name of a programming language! " );
     name = keyboard.nextLine();
 
-    if ( name.compareTO("c++") < 0 )
+    if ( name.compareTo("c++") < 0 )
       System.out.println( name + " comes BEFORE c++" );
+
+    if ( name.compareTo("c++") == 0 )
+      System.out.println( "c++ isn't a made-up language!" );
+
+    if ( name.compareTo("c++") > 0 )
+      System.out.println( name + " comes AFTER c++" );
+
+    if ( name.compareTo("go") < 0 )
+      System.out.println( name + " comes BEFORE go" );
+
+    if ( name.compareTo("go") == 0 )
+      System.out.println( "go isn't a made-up language!" );
+
+    if ( name.compareTo("go") > 0 )
+      System.out.println( name + " comes AFTER go" );
+
+    if ( name.compareTo("java") < 0 )
+      System.out.println( name + " comes BEFORE java" );
+
+    if ( name.compareTo("java") == 0 )
+      System.out.println( "Java isn't a made-up language!" );
+
+    if ( name.compareTo("java") > 0 )
+      System.out.println( name + " comes AFTER java" );
+
+    if ( name.compareTo("lisp") < 0 )
+      System.out.println( name + " comes BEFORE lisp" );
+
+    if ( name.compareTo("lisp") == 0 )
+      System.out.println( "lisp isn't a made-up language!" );
+
+    if ( name.compareTo("lisp") > 0 )
+      System.out.println( name + " comes AFTER lisp" );
+
+    if ( name.compareTo("python") < 0 )
+      System.out.println( name + " comes BEFORE python" );
+
+    if ( name.compareTo("python") == 0 )
+      System.out.println( "python isn't a made-up language!" );
+
+    if ( name.compareTo("python") > 0 )
+      System.out.println( name + " comes AFTER python" );
+
+    if ( name.compareTo("ruby") < 0 )
+      System.out.println( name + " comes BEFORE ruby" );
+
+    if ( name.compareTo("ruby") == 0 )
+      System.out.println( "ruby isn't a made-up language!" );
+
+    if ( name.compareTo("ruby") > 0 )
+      System.out.println( name + " comes AFTER ruby" );
+
+    if ( name.compareTo("visualbasics") < 0 )
+      System.out.println( name + " comes BEFORE visualbasics" );
+
+    if ( name.compareTo("visualbasics") == 0 )
+      System.out.println( "visualbasics isn't a made-up language" );
+
+    if ( name.compareTo("visualbasics") > 0 )
+      System.out.println( name + " comes AFTER visualbasics" );
 
 
   }
 }
+
